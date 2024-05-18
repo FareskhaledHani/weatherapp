39361bce9f460ca2a7f587e3791f4b8e29db6a06
finalpro <fareskhaled11114@gmail.com>
2024-05-18 16:42:29 +0300

    weatherapp

---
+++ lib/cubits/wether_cubits/wether_state.dart
@@ -0,0 +1,5 @@
+abstract class WeatherState{}
+class WeatherIntial extends WeatherState{}
+class WeatherLoading extends WeatherState{}
+class WeatherSucces extends WeatherState{}
+class WeatherFailure extends WeatherState{}
